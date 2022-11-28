92fbc39a5b7465b5ecfd6850a7dec8ab3578ee01
Alyona <borisovsk_a@mail.ru>
2022-11-28 14:18:01 +0300

    Add onClick listener

--- app/src/main/java/com/example/testandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/testandroidapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.testandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActLog", "onCreate")
+        var tx = findViewById<TextView>(R.id.myTextView)
+        var bt = findViewById<Button>(R.id.myButton)
+        bt.setOnClickListener {
+            tx.text = "Button pressed!"
+            Log.d("MainActLog", "Button pressed")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
