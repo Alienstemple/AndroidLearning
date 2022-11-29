937ee1d4c6edca27048ba6bafa8ae73799e0907b
Alyona <borisovsk_a@mail.ru>
2022-11-29 06:43:48 +0300

    feat: Add Constance

---
+++ app/src/main/java/com/example/testandroidapp/constance/Constance.kt
@@ -0,0 +1,15 @@
+package com.example.testandroidapp.constance
+
+object Constance {
+
+    const val SIGN_IN_STATE = "sign_in"
+    const val SIGN_UP_STATE = "sign_up"
+    const val SIGN_STATE = "sign_state"
+    const val REQUEST_CODE_SIGN_IN = 1212
+    const val REQUEST_CODE_SIGN_UP = 2121
+
+    const val LOGIN = "login"
+    const val NAME = "name"
+    const val NAME2 = "name2"
+    const val NAME3 = "name3"
+}
