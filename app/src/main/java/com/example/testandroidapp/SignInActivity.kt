b406f1d0833982d91c28d7607ba54b1033ee754d
Alyona <borisovsk_a@mail.ru>
2022-12-01 10:58:34 +0300

    feat: Send sms from Intint

--- app/src/main/java/com/example/testandroidapp/SignInActivity.kt
+++ app/src/main/java/com/example/testandroidapp/SignInActivity.kt
@@ -1,16 +1,23 @@
 package com.example.testandroidapp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.testandroidapp.constance.Constance
 import com.example.testandroidapp.databinding.ActivitySignInBinding
 import kotlin.random.Random
 
 class SignInActivity : AppCompatActivity() {
+    val TAG = "SignInActLog"
     lateinit var signInBinding: ActivitySignInBinding
     private var greetingsList = listOf("Привет 1", "Добрый день", "Хорошего дня", "Привет 2", "Привет 3")
 
@@ -22,8 +29,13 @@ class SignInActivity : AppCompatActivity() {
         // Random Greeting.
         signInBinding.greetingTextView.text = greetingsList.get(Random.nextInt(0, greetingsList.size))
 
+        // initialize variables
         val login = intent.getStringExtra(Constance.LOGIN).toString()  // getIntent set login from ActivityMain to login TextView
-        Log.d("SignInActLog", "Passed from ActivityMain login is " + login)
+        val phone = intent.getStringExtra(Constance.PHONE).toString()
+//        val msgText  // TODO
+
+
+        Log.d(TAG, "Passed from ActivityMain login is " + login)
         signInBinding.login.text = login
         signInBinding.phone.text = Constance.SAMPLE_PHONE
 
@@ -39,6 +51,34 @@ class SignInActivity : AppCompatActivity() {
             setResult(RESULT_OK, intent)
             finish()
         }
+
+        signInBinding.smsButton.setOnClickListener {
+
+            checkSmsPermission()
+
+            val testPhone = "+79160630604"
+            val msgText = "Hello! Sample sms text"
+
+            try {
+                val smsManager = SmsManager.getDefault()
+//                smsManager = this.getSystemService(SmsManager::class.java)  // smsManager null!
+//                smsManager = applicationContext.getSystemService(SmsManager::class.java)
+//                smsManager = SmsManager.getDefault()
+
+                val smsIntent = Intent(Intent.ACTION_VIEW)
+                smsIntent.data = Uri.parse("sms:$testPhone")
+                startActivity(smsIntent)
+
+//                smsManager.sendTextMessage(testPhone, null, msgText, null, null)
+
+//                Toast.makeText(applicationContext, "Message Sent", Toast.LENGTH_LONG).show()
+
+            } catch (e: Exception) {
+                Toast.makeText(applicationContext, "Please enter all the data.."+e.message.toString(), Toast.LENGTH_LONG)
+                    .show()
+                Log.d(TAG, "Error while sms sending: ${e.message.toString()}")
+            }
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -53,5 +93,11 @@ class SignInActivity : AppCompatActivity() {
 
         val greetingText = savedInstanceState?.getCharSequence("savedText")
         signInBinding.greetingTextView.text = greetingText
+    }
+
+    private fun checkSmsPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), 101)
+        }
     }
 }
